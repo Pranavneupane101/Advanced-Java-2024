d955d6ffb39ce8832a59800dbe3b6bb21d721e14
Pranavneupane101 <neupanepranav40@gmail.com>
2024-05-31 17:30:02 +0545

    next program done!!

---
+++ corejava/Myinfo.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class Myinfo {
+	public static void main(String[] args) {
+		System.out.println("My name is pranav Neupane.");
+		System.out.println("my roll number is KAN077BCT057");
+		System.out.println("programming is my passion");
+		System.out.println("Cow is our national animal.");
+		}
+
+}
