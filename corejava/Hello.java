02aac67868cdbef21bf2b8f4ffa3c6adac2de874
Pranavneupane101 <neupanepranav40@gmail.com>
2024-05-31 17:28:06 +0545

    My first JAVA program

---
+++ corejava/Hello.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("hello java");
+	System.out.println("k xah guys");
+	System.out.println("thik xah");
+}
+}
+
